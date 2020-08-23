2da535cb62ae6d4064f425308bf67bb3ba15937d
Quintin Dwight <qhdwight@gmail.com>
2020-08-23 01:52:48 -0700

    Frogger sprites and animation

--- Assets/FroggerBehavior.cs
+++ Assets/FroggerBehavior.cs
@@ -1,6 +1,22 @@
-﻿using UnityEngine;
+﻿using JetBrains.Annotations;
+using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class FroggerBehavior : MonoBehaviour
 {
+    private Animator m_Animator;
 
+    private void Start() => m_Animator = GetComponent<Animator>();
+
+    [UsedImplicitly]
+    public void OnMove(InputValue input)
+    {
+        Vector3 move = input.Get<Vector2>();
+        if (move.sqrMagnitude < Mathf.Epsilon) return;
+
+        Transform t = transform;
+        t.rotation = Quaternion.AngleAxis(Mathf.Rad2Deg * Mathf.Atan2(-move.x, move.y), Vector3.forward);
+        t.Translate(move, Space.World);
+        m_Animator.Play("Hop");
+    }
 }
